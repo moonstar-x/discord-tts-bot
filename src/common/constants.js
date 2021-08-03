0914dda1b496adde2a2c787b05d46c536bea3e48
moonstar-x <christian.lopez99@outlook.com>
2021-08-02 22:06:58 -0500

    Cleaned up constants file.

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,7 +1,3 @@
-const MAX_EMBED_FIELD_SIZE = 1024;
-
-const TTS_MAX_CHARS = 200;
-
 const MESSAGE_EMBED = {
   color: '#20B2AA',
   langThumbnail: 'https://i.imgur.com/QbNXO4q.jpg',
@@ -10,25 +6,6 @@ const MESSAGE_EMBED = {
   helpURL: 'https://github.com/moonstar-x/discord-tts-bot/issues'
 };
 
-const ACTIVITY_TYPE = {
-  playing: 'PLAYING',
-  streaming: 'STREAMING',
-  listening: 'LISTENING',
-  watching: 'WATCHING'
-};
-
-const TTS_ENGINES = {
-  google: 'google',
-  aeiou: 'aeiou'
-};
-
-const AEIOU_API_URL = 'http://tts.cyzon.us';
-
 module.exports = {
-  MAX_EMBED_FIELD_SIZE,
-  TTS_MAX_CHARS,
-  MESSAGE_EMBED,
-  ACTIVITY_TYPE,
-  TTS_ENGINES,
-  AEIOU_API_URL
+  MESSAGE_EMBED
 };
