d333b5db73d360f299d4957b9ecd0ce1027835ba
moonstar-x <christian.lopez99@outlook.com>
2022-01-18 20:47:30 -0500

    Added a testing_guild_id setting and deploy commands on startup.

--- src/app.js
+++ src/app.js
@@ -10,7 +10,8 @@ const config = new ConfigProvider({
     OWNER_ID: null,
     OWNER_REPORTING: false,
     PRESENCE_REFRESH_INTERVAL: 15 * 60 * 1000, // 15 Minutes
-    DISCONNECT_TIMEOUT: 5 * 60 * 1000 // 5 Minutes
+    DISCONNECT_TIMEOUT: 5 * 60 * 1000, // 5 Minutes,
+    TESTING_GUILD_ID: null
   },
   types: {
     TOKEN: 'string',
@@ -18,7 +19,8 @@ const config = new ConfigProvider({
     OWNER_ID: ['string', 'null'],
     OWNER_REPORTING: 'boolean',
     PRESENCE_REFRESH_INTERVAL: ['number', 'null'],
-    DISCONNECT_TIMEOUT: ['number', 'null']
+    DISCONNECT_TIMEOUT: ['number', 'null'],
+    TESTING_GUILD_ID: ['string', 'null']
   }
 });
 
@@ -37,6 +39,7 @@ const client = new TTSClient({
       'up for {uptime}.'
     ]
   },
+  testingGuildID: config.get('TESTING_GUILD_ID'),
   intents: ['GUILD_MESSAGES', 'GUILD_MEMBERS', 'GUILDS', 'GUILD_VOICE_STATES']
 });
 
@@ -54,8 +57,13 @@ client.registry
   ])
   .registerCommandsIn(path.join(__dirname, './commands'));
 
-client.on('ready', () => {
+client.on('ready', async() => {
   client.initializeDependencies();
+
+  if (config.get('TESTING_GUILD_ID')) {
+    client.deployer.rest.setToken(config.get('TOKEN'));
+    await client.deployer.deployToTestingGuild();
+  }
 });
 
 client.login(config.get('TOKEN'));
