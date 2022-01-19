a7c36cc7eeb1a72f8c33a2c39258c23f8a3e2173
moonstar-x <christian.lopez99@outlook.com>
2022-01-19 15:54:28 -0500

    Localized utils/channel.js

--- src/locales/en.js
+++ src/locales/en.js
@@ -2,6 +2,14 @@ const APP = {
   'app.message.deprecated': 'Commands have been turned into slash commands and you may not use them with the prefix anymore. You can run the command you tried to input by running: **/{commandName}**'
 };
 
+const ERROR = {
+  'error.channel.not_viewable': 'I cannot see your voice channel. Do I have enough permissions to view it?',
+  'error.channel.full': 'Your voice channel is full.',
+  'error.channel.not_joinable': 'I cannot join your voice channel. Do I have enough permissions to join it?',
+  'error.channel.not_speakable': 'I cannot speak in your voice channel. Do I have enough permissions to speak in it?'
+};
+
 module.exports = {
-  ...APP
+  ...APP,
+  ...ERROR
 };
--- src/utils/channel.js
+++ src/utils/channel.js
@@ -1,18 +1,18 @@
 const getCantConnectToChannelReason = (channel) => {
   if (!channel.viewable) {
-    return 'I cannot see your voice channel. Do I have enough permissions to view it?';
+    return 'error.channel.not_viewable';
   }
 
   if (channel.full) {
-    return 'Your voice channel is full.';
+    return 'error.channel.full';
   }
 
   if (!channel.joinable) {
-    return 'I cannot join your voice channel. Do I have enough permissions to join it?';
+    return 'error.channel.not_joinable';
   }
 
   if (channel.type === 'GUILD_VOICE' && !channel.speakable) {
-    return 'I cannot speak in your voice channel. Do I have enough permissions to speak in it?';
+    return 'error.channel.not_speakable';
   }
 };
 
