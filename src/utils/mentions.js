ebf99eb789ee8c788352b5f4836d63b8bf23d98b
moonstar-x <christian.lopez99@outlook.com>
2022-02-03 11:06:38 -0500

    Changed how emoji names are parsed in mentions util.

--- src/utils/mentions.js
+++ src/utils/mentions.js
@@ -1,6 +1,6 @@
 const { MessageMentions: { USERS_PATTERN, CHANNELS_PATTERN, ROLES_PATTERN } } = require('discord.js');
 
-const CUSTOM_EMOJI_PATTERN = /<:.*?:(\d{17,19})>/g;
+const CUSTOM_EMOJI_PATTERN = /<:(.*?):(\d{17,19})>/g;
 
 const cleanMemberMentions = (message, members) => {
   return message.replace(USERS_PATTERN, (_, id) => members.get(id).displayName);
@@ -14,17 +14,17 @@ const cleanRoleMentions = (message, roles) => {
   return message.replace(ROLES_PATTERN, (_, id) => roles.get(id).name);
 };
 
-const cleanEmojis = (message, emojis) => {
-  return message.replace(CUSTOM_EMOJI_PATTERN, (_, id) => emojis.get(id).name);
+const cleanEmojis = (message) => {
+  return message.replace(CUSTOM_EMOJI_PATTERN, (_, name) => name);
 };
 
-const cleanMessage = (message, { members, channels, roles, emojis }) => {
+const cleanMessage = (message, { members, channels, roles }) => {
   let clean = message;
 
   clean = cleanMemberMentions(clean, members);
   clean = cleanChannelMentions(clean, channels);
   clean = cleanRoleMentions(clean, roles);
-  clean = cleanEmojis(clean, emojis);
+  clean = cleanEmojis(clean);
 
   return clean;
 };
